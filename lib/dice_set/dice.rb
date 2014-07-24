6a8515c44549e2e9f55b2adae7181d0da2b96679
Rafaël Gonzalez <rafaelgonzalez@users.noreply.github.com>
2014-07-25 00:52:11 +0200

    Add YARD documentation

--- lib/dice_set/dice.rb
+++ lib/dice_set/dice.rb
@@ -1,11 +1,22 @@
 module DiceSet
+
+  # The Dice class is the representation of a dice.
   class Dice
+
+    # @!attribute [r] sides
+    # @return [Fixnum] the number of sides of the dice
+
     attr_reader :sides
 
+    # @param sides [Fixnum] the number of sides of the dice
     def initialize(sides)
       @sides = sides
     end
 
+    # Rolls multiple dices with the same number of sides.
+    # @param sides [Fixnum] the number of sides of the dices
+    # @param amount [Fixnum] the number of dices rolled
+    # @return [Fixnum] the total roll result
     def self.roll(sides, amount = 1)
       return 0 if amount.nil? || amount <= 0
 
@@ -14,6 +25,8 @@ module DiceSet
       end.inject(&:+)
     end
 
+    # Rolls the dice.
+    # @return [Fixnum] the roll result
     def roll
       self.class.roll(sides, 1)
     end
--- lib/dice_set/version.rb
+++ lib/dice_set/version.rb
@@ -1,3 +1,4 @@
 module DiceSet
+  # Current version number of the library
   VERSION = '0.0.1'
 end
