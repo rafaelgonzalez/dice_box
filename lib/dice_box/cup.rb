5bbc179618fbc6d9266d00387477e189ae8c23c1
Rafaël Gonzalez <rafaelgonzalez@users.noreply.github.com>
2014-08-10 17:48:55 +0200

    WIP DiceBox::Cup.new_from_notation

--- lib/dice_box/cup.rb
+++ lib/dice_box/cup.rb
@@ -15,19 +15,43 @@ module DiceBox
     alias_method :rolled, :result
     alias_method :rolled_value, :result
 
+    attr_accessor :operations
+
     # @param dices [Array] an Array of Dices to put in the Cup
     def initialize(dices = [])
       @dices = dices
       @rolled_sides = []
+      @operations = {}
+    end
+
+    # Instantiates a Cup and adds dices to it from the given the notation
+    #
+    # @param notation [String] the dice notation
+    # @return [DiceBox::Cup] the Cup instance
+    def self.new_from_notation(notation)
+      notation_hash = DiceBox::Dice::Notation.parse(notation)
+
+      dices = notation_hash[:dices].times.map do
+        Dice.new(notation_hash[:sides])
+      end
+
+      cup = new(dices)
+      cup.operations = notation_hash[:operations]
+
+      cup
     end
 
     # Rolls all the Dices in the Cup
+    # Applies eventual operations assigned to the Cup
+    #
     # @return [Integer] the sum of the rolled Dices
     def roll
-      @result = dices.map { |dice| dice.roll }.reduce(&:+)
+      roll = dices.map { |dice| dice.roll }.reduce(&:+)
+      roll = apply_operations_to_roll(roll, operations)
+
       @rolled_sides = dices.map(&:rolled_side)
 
-      result
+      @result = roll
     end
 
     # Returns the highest value the Cup can roll
@@ -43,5 +67,15 @@ module DiceBox
       dices.map(&:minimum).reduce(:+)
     end
     alias_method :min, :minimum
+
+    private
+
+    def apply_operations_to_roll(roll, operations)
+      operations.each do |suffix|
+        roll = roll.public_send(suffix[:operation], suffix[:number])
+      end
+
+      roll
+    end
   end
 end
