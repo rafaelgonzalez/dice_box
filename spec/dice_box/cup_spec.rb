00ea3690256ebf26f5dca220851d0610d439c017
Rafaël Gonzalez <rafaelgonzalez@users.noreply.github.com>
2015-01-11 08:57:44 +1100

    Rewrite DiceBox::Cup specs

--- spec/dice_box/cup_spec.rb
+++ spec/dice_box/cup_spec.rb
@@ -1,8 +1,10 @@
 describe DiceBox::Cup do
+  let(:cup) { described_class.new(dices) }
   let(:dices) { [] }
-  subject { described_class.new(dices) }
 
   describe '#roll' do
+    subject { cup.roll }
+
     let(:dice_1) { DiceBox::Dice.new(6) }
     let(:dice_2) { DiceBox::Dice.new(4) }
     let(:dice_3) { DiceBox::Dice.new(12) }
@@ -13,26 +15,28 @@ describe DiceBox::Cup do
       expect(dice_2).to receive(:roll).and_return(1)
       expect(dice_3).to receive(:roll).and_return(10)
 
-      expect(subject.roll).to eql 14
+      is_expected.to eql 14
     end
   end
 
   describe '#result' do
+    subject { cup.rolled }
+
     let(:dice_1) { DiceBox::Dice.new(6) }
     let(:dice_2) { DiceBox::Dice.new(4) }
     let(:dice_3) { DiceBox::Dice.new(12) }
     let(:dices) { [dice_1, dice_2, dice_3] }
 
     it 'returns the sum of the last rolled values' do
-      rolled_value = subject.roll
+      rolled_value = cup.roll
 
-      expect(subject.result).to eql rolled_value
+      is_expected.to eql rolled_value
     end
 
     it 'does not change value if dices are rerolled' do
-      rolled_value = subject.roll
+      rolled_value = cup.roll
 
-      expect(subject.result).to eql rolled_value
+      is_expected.to eql rolled_value
 
       expect(dice_1).to receive(:roll).and_return(1)
       expect(dice_2).to receive(:roll).and_return(1)
@@ -42,38 +46,42 @@ describe DiceBox::Cup do
       dice_2.roll
       dice_3.roll
 
-      expect(subject.result).to eql rolled_value
+      is_expected.to eql rolled_value
     end
   end
 
   describe '#rolled_sides' do
+    subject { cup.rolled_sides }
+
     let(:dice_1) { DiceBox::Dice.new(6) }
     let(:dice_2) { DiceBox::Dice.new(4) }
     let(:dice_3) { DiceBox::Dice.new(12) }
     let(:dices) { [dice_1, dice_2, dice_3] }
 
     it 'returns an Array of Sides' do
-      expect(subject.rolled_sides).to be_an(Array)
-      expect(subject.rolled_sides).to be_empty
+      is_expected.to be_an(Array)
+      is_expected.to be_empty
 
-      subject.roll
+      cup.roll
 
-      expect(subject.rolled_sides).to be_an(Array)
-      expect(subject.rolled_sides).to be_an(Array)
+      is_expected.to be_an(Array)
     end
 
     it 'does not change if dices are not rolled with the cup' do
-      subject.roll
-      rolled_sides = subject.rolled_sides
+      cup.roll
+      rolled_sides = cup.rolled_sides
 
       dice_1.roll
       dice_2.roll
       dice_3.roll
-      expect(subject.rolled_sides).to eql rolled_sides
+
+      is_expected.to eql rolled_sides
     end
   end
 
   describe '#maximum' do
+    subject { cup.maximum }
+
     let(:dices) do
       [
         DiceBox::Dice.new(6),
@@ -82,12 +90,8 @@ describe DiceBox::Cup do
       ]
     end
 
-    subject { DiceBox::Cup.new(dices) }
-
     context 'with classical dices' do
-      it 'returns the highest value the cup can roll' do
-        expect(subject.maximum).to eql 126
-      end
+      it { is_expected.to eql 126 }
     end
 
     context 'with dices with changed side values' do
@@ -97,13 +101,13 @@ describe DiceBox::Cup do
         dices[2].sides[0].value = 112
       end
 
-      it 'returns the highest value the cup can roll' do
-        expect(subject.maximum).to eql 266
-      end
+      it { is_expected.to eql 266 }
     end
   end
 
   describe '#minimum' do
+    subject { cup.minimum }
+
     let(:dices) do
       [
         DiceBox::Dice.new(6),
@@ -112,12 +116,8 @@ describe DiceBox::Cup do
       ]
     end
 
-    subject { DiceBox::Cup.new(dices) }
-
     context 'with classical dices' do
-      it 'returns the lowest value the cup can roll' do
-        expect(subject.minimum).to eql 3
-      end
+      it { is_expected.to eql 3 }
     end
 
     context 'with dices with changed side values' do
@@ -132,9 +132,7 @@ describe DiceBox::Cup do
         end
       end
 
-      it 'returns the lowest value the cup can roll' do
-        expect(subject.minimum).to eql 228
-      end
+      it { is_expected.to eql 228 }
     end
   end
 end
